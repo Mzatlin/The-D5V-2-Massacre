47f538c5d7eecbd161581593d869eb33dacfdd92
Mzatlin <michael.zatlin@yahoo.com>
2020-12-07 20:34:57 -0800

    Added Sprint mechanic player can use by holding down the shift key while the sprint bar is filled.

---
+++ ScreamFM/Assets/Scripts/Player/PlayerSprint.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprint : MonoBehaviour
+{
+    IPlayerState state => GetComponent<IPlayerState>();
+    IMovement movement => GetComponent<IMovement>();
+    [SerializeField]
+    float sprintAmount = 100f;
+    [SerializeField]
+    float moveSpeedMultiplier = 2f;
+    float baseMovementSpeed;
+    bool isSpriting = false;
+    public float SprintAmount => sprintAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(movement != null)
+        {
+            baseMovementSpeed = movement.MoveSpeed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state != null && state.PlayerState.IsPlayerReady())
+        {
+            TrySprint();
+        }
+
+        if (isSpriting)
+        {
+            sprintAmount -= 30 * Time.deltaTime;
+        }
+        else
+        {
+            sprintAmount += 10 * Time.deltaTime;
+        }
+        sprintAmount = Mathf.Clamp(sprintAmount, 0, 100);
+    }
+
+    void TrySprint()
+    {
+        if (sprintAmount > 1 && Input.GetKey(KeyCode.LeftShift))
+        {
+            if(movement != null && !isSpriting)
+            {
+                isSpriting = true;
+                movement.MoveSpeed *= moveSpeedMultiplier;   
+            }
+        }
+        else
+        {
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                isSpriting = false;
+            }
+            movement.MoveSpeed = baseMovementSpeed;
+        }
+        
+    }
+}
---
+++ ScreamFM/Assets/Scripts/UI/SprintBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SprintBar : MonoBehaviour
+{
+    PlayerSprint sprint => GetComponent<PlayerSprint>();
+    [SerializeField]
+    Slider sprintSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sprint != null)
+        {
+            sprintSlider.value = sprint.SprintAmount/100;
+        }
+    }
+}
