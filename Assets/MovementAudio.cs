896d0272a17be67e7688278eb14f3e55ce86a983
jgamelin <jgam_compose@yahoo.com>
2020-12-10 06:08:21 -0800

    Added 'Movement Audio' script to Player Graphics. Added Walking, Running, and Climbing sound Animation Events to the Player Animation

---
+++ Assets/MovementAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAudio : MonoBehaviour
+{
+    [SerializeField] GameObject mainCamera;
+    void WalkingAudio()
+    {
+        AkSoundEngine.SetState("Locomotion", "Walking");
+        AkSoundEngine.PostEvent("Play_Footsteps", mainCamera);
+    }
+
+    void RunningAudio()
+    {
+        AkSoundEngine.SetState("Locomotion", "Running");
+        AkSoundEngine.PostEvent("Play_Footsteps", mainCamera);
+    }
+
+    void ClimbingAudio()
+    {
+        AkSoundEngine.PostEvent("Play_LadderClimb", mainCamera);
+    }
+}
