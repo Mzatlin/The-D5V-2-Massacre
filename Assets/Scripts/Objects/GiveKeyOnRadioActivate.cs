41be4ed61f8c9f49002ef9eb8e00993543ecf93b
Mzatlin <michael.zatlin@yahoo.com>
2020-12-10 16:39:32 -0800

    All ground and floor assets have been added plus more misc art. Added a script that gives the player a key after a certain amount of radios have been activated.

---
+++ Assets/Scripts/Objects/GiveKeyOnRadioActivate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveKeyOnRadioActivate : MonoBehaviour
+{
+    public RadioCompletionListSO radio;
+    public KeyListSO keys;
+    public string keyName;
+    IRadioMinigame minigame => GetComponent<IRadioMinigame>();
+    [SerializeField]
+    int amountForKey = 4;
+    int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (minigame != null)
+        {
+            minigame.OnComplete += HandleComplete;
+        }
+    }
+
+    private void HandleComplete()
+    {
+        if (radio != null)
+        {
+            foreach (KeyValuePair<RadioSO, bool> complete in radio.radioStatuses)
+            {
+                if (complete.Value == true)
+                {
+                    counter++;
+                }
+            }
+
+            if(counter == amountForKey)
+            {
+                if (!keys.IsKeyCollected(keyName))
+                {
+                    keys.keyStatuses[keyName] = true;
+                }
+            }
+        }
+    }
+}
