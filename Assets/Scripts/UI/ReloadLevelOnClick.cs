3cdcb8e6e9b5fa9c78df53dd523d20295012785b
Mzatlin <michael.zatlin@yahoo.com>
2020-12-11 16:05:12 -0800

    Added a title screen menu and linked death and end credit buttons to it.

--- Assets/Scripts/UI/ReloadLevelOnClick.cs
+++ Assets/Scripts/UI/ReloadLevelOnClick.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ReloadLevelOnClick : MonoBehaviour
 {
+    public string levelName = "";
     public void ReloadLevel()
     {
         GameObject mainCamera = FindObjectOfType<Camera>().gameObject;
         AkSoundEngine.PostEvent("StopAll", mainCamera);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        SceneManager.LoadScene(levelName, LoadSceneMode.Single);
     }
 }
