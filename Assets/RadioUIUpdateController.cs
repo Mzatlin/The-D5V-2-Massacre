aeb0508e1c2ccf296033b6378ef71573af0bd479
Mzatlin <michael.zatlin@yahoo.com>
2021-01-15 23:45:16 -0800

    Created a radio counter UI that tracks how many radios the player fixed.

---
+++ Assets/RadioUIUpdateController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class RadioUIUpdateController : MonoBehaviour
+{
+    public TextMeshProUGUI countText;
+    public RadioCompletionListSO radioCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (radioCount != null)
+        {
+            radioCount.OnRadioComplete += UpdateCountOnComplete;
+        }
+        countText.text = "0/5";
+    }
+
+    private void OnDestroy()
+    {
+        if (radioCount != null)
+        {
+            radioCount.OnRadioComplete -= UpdateCountOnComplete;
+        }
+    }
+
+    private void UpdateCountOnComplete(Transform obj)
+    {
+        countText.text = radioCount.completeCount + "/5";
+    }
+}
