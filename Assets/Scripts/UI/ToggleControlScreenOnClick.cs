5de119475b91b9ac8ff9ddb1551217cda45121e7
Mzatlin <michael.zatlin@yahoo.com>
2021-01-29 23:53:34 -0800

    Added a basic "Controls" section to the title screen and pause menu that lists all the keyboard controls.

---
+++ Assets/Scripts/UI/ToggleControlScreenOnClick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleControlScreenOnClick : MonoBehaviour
+{
+    public Canvas canvasToToggle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(canvasToToggle != null)
+        {
+            canvasToToggle.enabled = false;
+        }
+        else
+        {
+            Debug.Log("No Canvas Attached!");
+        }
+    }
+
+    public void OnClickToggle()
+    {
+        if(canvasToToggle != null)
+        {
+            ToggleCanvas();
+        }
+    }
+
+    void ToggleCanvas()
+    {
+        if (canvasToToggle.enabled)
+        {
+            canvasToToggle.enabled = false;
+        }
+        else
+        {
+            canvasToToggle.enabled = true;
+        }
+    }
+}
