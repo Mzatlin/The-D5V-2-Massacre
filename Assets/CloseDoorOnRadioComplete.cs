4079a1323ec0292608158b398c6c1a3f6198631f
Mzatlin <michael.zatlin@yahoo.com>
2021-02-20 23:20:51 -0800

    All paths to radio now close when the player enters the room with all 5 radios complete

---
+++ Assets/CloseDoorOnRadioComplete.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoorOnRadioComplete : MonoBehaviour
+{
+    public RadioCompletionListSO radio;
+    public List<GameObject> doors = new List<GameObject>();
+    [SerializeField]
+    LayerMask playerMask;
+    void CloseDoor()
+    {
+
+        foreach (GameObject door in doors)
+        {
+            if (door != null)
+            {
+                var doorToggle = door.GetComponent<IDoor>();
+                if (doorToggle != null)
+                {
+                    doorToggle.CloseDoor();
+                }
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((playerMask & 1 << collision.gameObject.layer) != 0)
+        {
+            if (radio != null && radio.HasWon())
+            {
+                CloseDoor();
+            }
+        }
+    }
+}
