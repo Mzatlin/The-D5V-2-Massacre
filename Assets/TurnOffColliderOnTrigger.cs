81896f4e9c9cb3e6c63db14c030bf55b1e25924a
Mzatlin <michael.zatlin@yahoo.com>
2021-03-20 01:28:04 -0700

    Disabled radio room vent's collider when ending is triggered to prevent unwanted animation from playing for the time being

---
+++ Assets/TurnOffColliderOnTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffColliderOnTrigger : HandlePlayerTriggerEventBase
+{
+    public GameObject objectCollider;
+    Collider2D colliderToVanish;
+
+    protected override void Start()
+    {
+        base.Start();
+        if (objectCollider != null)
+        {
+            colliderToVanish = objectCollider.GetComponent<Collider2D>();
+            if(colliderToVanish != null)
+                  colliderToVanish.enabled = true;
+        }
+    }
+
+    protected override void HandleTrigger()
+    {
+        if (colliderToVanish != null)
+        {
+            colliderToVanish.enabled = false;
+        }
+    }
+}
